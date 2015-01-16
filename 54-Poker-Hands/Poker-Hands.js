e6dcc9d908082838329e9f870c13d57e099e0f99
veneling <v.grozev@outlook.com>
2015-01-16 07:24:03 +0200

    update poker-hands

--- 54-Poker-Hands/Poker-Hands.js
+++ 54-Poker-Hands/Poker-Hands.js
@@ -1,17 +1,12 @@
-function readData(fileName) {
+function readDataSync(fileName) {
 	fs = require('fs');
 	path = require('path');
-
 	var filePath = path.join(__dirname, fileName);
-	var encoding_ = 'utf8';
-	fs.readFile(filePath, encoding_, function(err, data) {
-		if (err) {
-			console.log(err);
-		} else {
-			return x = data.split("\r\n");;
-		}
-	});
+
+	var contents = fs.readFileSync(filePath);
+	return contents;
+
 }
 
-var a = readData('_poker.txt');
-console.log(a);
+var a = readDataSync('_poker.txt');
+console.log(a.toString('utf8'));
