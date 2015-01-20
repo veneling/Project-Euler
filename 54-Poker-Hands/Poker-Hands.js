02f1f2264cccc5921ae2e77fc943cfd74e070367
veneling <veneling@users.noreply.github.com>
2015-01-20 11:56:20 +0200

    Add asynchronous reading of .txt file with nodejs
    Won't be used but good to have it as an example

--- 54-Poker-Hands/Poker-Hands.js
+++ 54-Poker-Hands/Poker-Hands.js
@@ -1,3 +1,19 @@
+function readData(fileName) {
+	fs = require('fs');
+	path = require('path');
+
+	var filePath = path.join(__dirname, fileName);
+	var encoding_ = 'utf8';
+	fs.readFile(filePath, encoding_, function(err, data) {
+		if (err) {
+			console.log(err);
+		} else {
+			return x = data.split("\r\n");
+			;
+		}
+	});
+}
+
 function readDataSync(fileName) {
 	fs = require('fs');
 	path = require('path');
